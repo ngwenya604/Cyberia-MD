72c0d9c4ed016aa6b6add7b24367bb2acb892b42
SungSu-ho <ryanbaartman12@gmail.com>
2025-08-21 00:43:10 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
-};
